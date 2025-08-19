36d66c5fc250bfed529a22db6b64889790fcdb65
issel <isselcode@gmail.com>
2025-08-19 01:53:56 -0600

    modelo para obtener las cantidades desde firebase

---
+++ lib/src/models/firebase_model.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../../core/app/enums.dart';
+import '../entities/product_entity.dart';
+
+class FirebaseModel {
+
+  FirebaseFirestore firestore;
+
+  FirebaseModel({
+    required this.firestore
+  });
+
+  // Convierte el doc totals a una entidad para un producto dado
+  ProductEntity mapTotalsToEntity(
+      DocumentSnapshot<Map<String, dynamic>> totalsDoc,
+      ProductType type,
+      ) {
+    final data = totalsDoc.data() ?? const {};
+
+    final stockMap = Map<String, dynamic>.from(data['stock'] ?? {});
+    final updatedMap = Map<String, dynamic>.from(data['lastUpdated'] ?? {});
+
+    final quantity = (stockMap[type.name] as num?)?.toInt() ?? 0;
+
+    final ts = updatedMap[type.name];
+    final lastUpdated = ts is Timestamp
+        ? ts.toDate()
+        : DateTime.fromMillisecondsSinceEpoch(0);
+
+    return ProductEntity(
+      type: type,
+      quantity: quantity,
+      lastUpdated: lastUpdated,
+    );
+  }
+
+// Cargar los 3 desde el único doc
+  Future<List<ProductEntity>> fetchAllProductsOptionB() async {
+    final db = FirebaseFirestore.instance;
+    final doc = await db.doc('inventory/totals').get();
+    return ProductType.values.map((t) => mapTotalsToEntity(doc, t)).toList();
+  }
+
+}
