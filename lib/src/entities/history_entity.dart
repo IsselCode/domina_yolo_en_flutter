9c51d6fcdf0653674b3d6ddeca2f8c532d4e9280
issel <isselcode@gmail.com>
2025-08-20 23:09:51 -0600

    Modificar entidad del historial

--- lib/src/entities/history_entity.dart
+++ lib/src/entities/history_entity.dart
@@ -1,15 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:domina_yolo_en_flutter/core/app/enums.dart';
+
 class HistoryProductEntity {
 
-  String productName;
-  int delta;
-  int balanceAfter;
+  ProductType productType;
+  String image;
+  DeltaType deltaType;
+  int quantity;
+  int signedQuantity;
   DateTime ts;
 
   HistoryProductEntity({
-    required this.productName,
-    required this.delta,
-    required this.balanceAfter,
+    required this.productType,
+    required this.deltaType,
+    required this.quantity,
+    required this.signedQuantity,
     required this.ts,
+    required this.image
   });
+
+  factory HistoryProductEntity.fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) {
+    final data = doc.data()!;
+
+    ProductType productType = ProductType.fromString(data["productId"])!;
+    DeltaType deltaType = DeltaType.fromString(data["deltaType"])!;
+
+    return HistoryProductEntity(
+      productType: productType,
+      deltaType: deltaType,
+      quantity: data["quantity"],
+      signedQuantity: data["signedQuantity"],
+      ts: (data["ts"] as Timestamp?)!.toDate(),
+      image: productImageByType[productType]!
+    );
+  }
 
 }
